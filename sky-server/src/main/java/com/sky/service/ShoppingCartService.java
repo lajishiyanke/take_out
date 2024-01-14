1a0c46edfce75c585cd7ae43bfd84d02ad43149e
w <trump@123.com>
2024-01-14 14:40:09 +0800

    购物车功能

---
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -0,0 +1,9 @@
+package com.sky.service;
+
+import com.sky.dto.ShoppingCartDTO;
+import org.springframework.stereotype.Service;
+
+
+public interface ShoppingCartService {
+    void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,65 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.awt.*;
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private SetmealMapper setmealMapper;
+    @Override
+    public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        //判断商品已经存在于购物车中？
+        ShoppingCart shoppingCart=new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+        Long currentId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(currentId);
+        List<ShoppingCart>list = shoppingCartMapper.list(shoppingCart);
+        if(list !=null&&list.size()>0){
+            //是则数量加1
+            ShoppingCart cart = list.get(0);
+            cart.setNumber(cart.getNumber()+1);
+            //update
+            shoppingCartMapper.updateByid(cart);
+        }else{
+            //否则插入数据，多的数据需要根据是菜品还是套餐去对应的表中查询
+            Long dishId = shoppingCartDTO.getDishId();
+            Long setmealId = shoppingCartDTO.getSetmealId();
+            if(dishId !=null){
+                //是菜品 把少的内容加上去
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setCreateTime(LocalDateTime.now());
+                shoppingCart.setNumber(1);
+            }else{
+                //是套餐
+                Setmeal setmeal = setmealMapper.getById(setmealId);
+                shoppingCart.setAmount(setmeal.getPrice());
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setCreateTime(LocalDateTime.now());
+                shoppingCart.setNumber(1);
+            }
+            shoppingCartMapper.insert(shoppingCart);
+        }
+    }
+}
